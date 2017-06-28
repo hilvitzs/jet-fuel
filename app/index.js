002d512c1f81d18ba60fe28eb924dfa32a012a76
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-06-28 15:45:19 -0600

    folders are prepending to the dom from the database

--- app/index.js
+++ app/index.js
@@ -1,15 +1,10 @@
 const addFolderBtn = $('.folder-submit')
+const folders = $('.folders')
 
 addFolderBtn.on('click', () => {
   let userInput = $('.folder-input')
-
-  $('.folders').prepend(`
-    <section class='folder'>
-      <img src='./assets/images/folder.png'/>
-      <p>${userInput.val()}</p>
-    </section>
-  `)
-
+  addFolder(userInput.val())
+  getFolders()
   userInput.val('')
 })
 
@@ -20,3 +15,30 @@ $('body').on('click', '.folder', () => {
     </section>
   `)
 })
+
+const addFolder = (input) => {
+  fetch('/api/v1/folders',
+  {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({ title: input })
+  })
+}
+
+const getFolders = () => {
+  fetch('/api/v1/folders',
+  {
+    method: 'GET',
+    headers: {'Content-Type': 'application/json'}
+  }).then(response => response.json())
+    .then(folders => prepend(folders))
+}
+
+const prepend = (array) => {
+  array.map(folder => {
+    return folders.prepend(`<section class='folder'>
+      <img src='./assets/images/folder.png'/>
+      <p>${folder.title}</p>
+    </section>`)
+  })
+}
