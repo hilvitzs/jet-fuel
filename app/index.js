b81444e5f3b1f6d13f4a66d5909a39f7c5a51204
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-06-28 08:54:39 -0600

    add append on folder click

--- app/index.js
+++ app/index.js
@@ -4,11 +4,19 @@ addFolderBtn.on('click', () => {
   let userInput = $('.folder-input')
 
   $('.folders').prepend(`
-    <section>
-      <img src='assets/images/folder.png'/>
+    <section class='folder'>
+      <img src='./assets/images/folder.png'/>
       <p>${userInput.val()}</p>
     </section>
   `)
 
   userInput.val('')
+})
+
+$('body').on('click', '.folder', () => {
+  $('.folder').append(`
+    <section class='link'>
+      <p>something</p>
+    </section>
+  `)
 })
