92526ff6513ed3f8c0ad2ef20dee88f0c5ad9ea9
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-07-20 21:10:16 -0600

    something

--- server.js
+++ server.js
@@ -10,7 +10,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('app'));
-app.get('/', (request, response) => response.sendFile(path.join(__dirname, './app/index.html')));
+app.get('/', (request, response) => {
+  if (location.protocol != 'https:') {
+    location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+    window.location.reload();
+  }
+  response.sendFile(path.join(__dirname, './app/index.html'));
+});
+
 
 
 //add new folder
